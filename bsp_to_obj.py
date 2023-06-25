51be324455f727bfe68020f511b2048e2846518a
Lennart Greifenhain <lennart.greifenhain@pixelbiotech.com>
2023-06-25 19:08:20 +0200

    clean-up

--- bsp_to_obj.py
+++ bsp_to_obj.py
@@ -3,7 +3,19 @@ import os
 from colored_radar_image import get_polygons
 
 
-def obj_from_bsp(bsp_path='beta/oddball_b1.bsp', pball_path='./pball'):
+def obj_from_bsp(bsp_path: str='beta/oddball_b1.bsp', pball_path: str='./pball') -> str:
+    """
+    converts a bsp file alongside with mean color per face to a .obj standard string
+    First line is a comment describing the file
+    Next lines are a variably sized block defining vertex positions
+    Next lines are a variably sized block defining faces as 1-based indices to the vertices
+    Next lines are a variably sized block of comments defining colors as 0-255 RGB values or RGBA for faces not
+     rendered in-game. These lines are ignored by regular .obj parsers
+    Last line is a comment defining the color index for each face
+    :param bsp_path: path to bsp file relative to pball/maps
+    :param pball_path: path to pball directory
+    :return: .obj file as string
+    """
     (polys, colors) = \
         get_polygons(os.path.join(
             pball_path, 'maps', bsp_path), pball_path)
@@ -11,7 +23,6 @@ def obj_from_bsp(bsp_path='beta/oddball_b1.bsp', pball_path='./pball'):
     obj_file = ''
 
     faces = []
-    # vertex_count = 0
 
     vertex_lines = []
     face_lines = []
@@ -22,24 +33,24 @@ def obj_from_bsp(bsp_path='beta/oddball_b1.bsp', pball_path='./pball'):
     flattened_verts = [v for poly in polys for v in poly.vertices]
     flattened_verts = tuple(map(tuple, flattened_verts))
     unique_verts = tuple(set(flattened_verts))
-    # tuple(unique_verts).index(tuple(flattened_verts[5]))
-    # save each vertex making up a polygon
+    # save each unique vertex
     for vert in unique_verts:
         v = [x / 1000 for x in vert]
         line = f'v {v[0]:.4f} {v[2]:.4f} {v[1]:.4f}\n'
         vertex_lines.append(line)
 
+    # define each face as 1-based indices to the vertices
     for poly in polys:
         tmp_faces = []
         for v in poly.vertices:
-            # vertex_count += 1
             tmp_faces.append(unique_verts.index(tuple(v)) + 1)
-            # v = [x/1000 for x in v]
-            # line = f'v {v[0]:.4f} {v[2]:.4f} {v[1]:.4f}\n'
-            # vertex_lines.append(line)
 
         faces.append({'verts': tmp_faces, 'tex_id': poly.tex_id})
-    # save each face as indices to the vertices
+
+
+    # break down each polygon into triangles by fan triangulation
+    # save each triangle as 1-based indices to the vertices
+    # define color indices as
     color_indices = []
 
     for face in faces:
@@ -51,16 +62,14 @@ def obj_from_bsp(bsp_path='beta/oddball_b1.bsp', pball_path='./pball'):
             face_lines.append(line)
             color_indices.append(face['tex_id'])
 
-            # line = '# '
-            # line += ' '.join([str(x) for x in colors[face['tex_id']]])
-            # line += '\n'
-            # color_lines.append(line)
-
+    # save each unique color as 0-255 RGB values or RGBA for faces not rendered in-game
     for color in colors:
         line = '# '
         line += ' '.join([str(x) for x in color])
         line += '\n'
         color_lines.append(line)
+
+    # save all color indices in one commented line
     color_index_line = '# '
     color_index_line += ' '.join([str(x) for x in color_indices])
     color_index_line += '\n'
@@ -75,8 +84,3 @@ def obj_from_bsp(bsp_path='beta/oddball_b1.bsp', pball_path='./pball'):
     print(f'color lines: {len(color_indices)}')
     print(f'color indices: {len(color_indices)}')
     return obj_file
-
-out = obj_from_bsp()
-
-with open('/home/lennart/Downloads/test7.obj', 'w') as f:
-    f.write(out)
