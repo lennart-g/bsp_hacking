461820ba3bc551b26bc8f39ef6c047f523f2fe26
Lennart Greifenhain <lennart.greifenhain@pixelbiotech.com>
2023-11-11 21:07:40 +0100

    make independent of caller working directory and comment out non-wireframe examples

--- example_renderer.py
+++ example_renderer.py
@@ -2,8 +2,12 @@ import os
 
 from radar_image import create_image
 
-pball_path = os.path.abspath("./pball")
+from inspect import getsourcefile
+from os.path import dirname, abspath
+current_path = dirname(abspath(getsourcefile(lambda:0)))
 
+# pball_path = os.path.abspath("./pball")
+pball_path = os.path.join(current_path, 'pball')
 create_image(
     pball_path,
     "/maps/beta/oddball_b1.bsp",
@@ -16,38 +20,38 @@ create_image(
     z_an=0.0,
 )
 
-create_image(
-    pball_path,
-    "/maps/beta/oddball_b1.bsp",
-    "all",
-    2,
-    "mode2.png",
-    max_resolution=1024,
-    x_an=0.0,
-    y_an=0.0,
-    z_an=0.0,
-)
-
-create_image(
-    pball_path,
-    "/maps/beta/oddball_b1.bsp",
-    "all",
-    1,
-    "mode1.png",
-    max_resolution=1024,
-    x_an=0.0,
-    y_an=0.0,
-    z_an=0.0,
-)
-
-create_image(
-    pball_path,
-    "/maps/beta/oddball_b1.bsp",
-    "all",
-    0,
-    "mode0.png",
-    max_resolution=1024,
-    x_an=0.0,
-    y_an=0.0,
-    z_an=0.0,
-)
+# create_image(
+#     pball_path,
+#     "/maps/beta/oddball_b1.bsp",
+#     "all",
+#     2,
+#     "mode2.png",
+#     max_resolution=1024,
+#     x_an=0.0,
+#     y_an=0.0,
+#     z_an=0.0,
+# )
+#
+# create_image(
+#     pball_path,
+#     "/maps/beta/oddball_b1.bsp",
+#     "all",
+#     1,
+#     "mode1.png",
+#     max_resolution=1024,
+#     x_an=0.0,
+#     y_an=0.0,
+#     z_an=0.0,
+# )
+#
+# create_image(
+#     pball_path,
+#     "/maps/beta/oddball_b1.bsp",
+#     "all",
+#     0,
+#     "mode0.png",
+#     max_resolution=1024,
+#     x_an=0.0,
+#     y_an=0.0,
+#     z_an=0.0,
+# )
