9b6f6696a0c82b8200b40d4460c27c848eae23d1
Lennart Greifenhain <lennart.greifenhain@pixelbiotech.com>
2023-11-12 17:37:49 +0100

    update bl_info version to 0.2.0

--- bsp_importer/__init__.py
+++ bsp_importer/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Experimental BSP Importer",
     "author": "Lennart G",
     "location": "File > Import > Quake 2 BSP (.bsp)",
-    "version": (0, 1, 0),
+    "version": (0, 2, 0),
     "blender": (2, 80, 0),
     "category": "Import-Export"
 }
